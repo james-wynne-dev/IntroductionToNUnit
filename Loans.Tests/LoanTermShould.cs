c6801f5a99aabe9c918a1a5fd0ddd99e8cb1c970
James Wynne <james.wynne@villagesoftware.co.uk>
2020-06-02 15:39:46 +0100

    Added test project and created first test

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,18 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+
+namespace Loans.Tests
+{
+    [TestFixture]
+    public class LoanTermShould
+    {
+        [Test]
+        public void ReturnTermInMonths()
+        {
+            // sut stands for system under test
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+        }
+    }
+}
