edb7e58adefc46d8d068884d8d27e8b70714768e
James Wynne <james.wynne@villagesoftware.co.uk>
2020-06-03 09:48:31 +0100

    Basic tests

--- Loans.Tests/LoanTermShould.cs
+++ Loans.Tests/LoanTermShould.cs
@@ -12,7 +12,43 @@ namespace Loans.Tests
             // sut stands for system under test
             var sut = new LoanTerm(1);
 
-            Assert.That(sut.ToMonths(), Is.EqualTo(12));
+            Assert.That(sut.ToMonths(), Is.EqualTo(12), "Custom Error Message");
+        }
+
+        [Test]
+        public void StoreYears()
+        {
+            var sut = new LoanTerm(1);
+
+            Assert.That(sut.Years, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RespectValueEquality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(1);
+
+            Assert.That(a, Is.EqualTo(b));
+        }
+
+        [Test]
+        public void RespectValueInequality()
+        {
+            var a = new LoanTerm(1);
+            var b = new LoanTerm(2);
+
+            Assert.That(a, Is.Not.EqualTo(b));
+        }
+
+        // Testing with floating point numbers
+        [Test]
+        public void Double()
+        {
+            double a = 1.0 / 3.0;
+
+            Assert.That(a, Is.EqualTo(0.33).Within(0.004));
+            Assert.That(a, Is.EqualTo(0.33).Within(10).Percent);
         }
     }
 }
