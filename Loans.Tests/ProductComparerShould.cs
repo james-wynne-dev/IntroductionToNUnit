9795c4240e7bd1854a1df8dc8572f3f92f3e408c
James Wynne <james.wynne@villagesoftware.co.uk>
2020-06-03 10:27:38 +0100

    testing collections

--- Loans.Tests/ProductComparerShould.cs
+++ Loans.Tests/ProductComparerShould.cs
@@ -1,10 +1,103 @@
-﻿using System;
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Loans.Tests
 {
-    class ProductComparerShould
+    public class ProductComparerShould
     {
+        [Test]
+        public void ReturnCorrectNumberOfComparisons()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Has.Exactly(3).Items);
+        }
+
+
+        // Tests that all the items in a collection are unique
+        [Test]
+        public void NotReturnDuplicateComparisons()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            Assert.That(comparisons, Is.Unique);
+        }
+
+        // Tests that the collection contains a specific item
+        // Need to know the result
+        [Test]
+        public void ReturnComparisonForFirstProduct()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            var expectedProduct = new MonthlyRepaymentComparison("a", 1, 643.28m);
+
+            Assert.That(comparisons, Does.Contain(expectedProduct));
+        }
+
+        [Test]
+        public void ReturnComparisonForFirstProduct_WithPartialKnowExpectedValues()
+        {
+            var products = new List<LoanProduct>
+            {
+                new LoanProduct(1, "a", 1),
+                new LoanProduct(2, "b", 2),
+                new LoanProduct(3, "c", 3)
+            };
+
+            var sut = new ProductComparer(new LoanAmount("USD", 200_000m), products);
+
+            List<MonthlyRepaymentComparison> comparisons =
+                sut.CompareMonthlyRepayments(new LoanTerm(30));
+
+            // Don't care about the expected monthly replayment, only that the product is there
+            // Note that the properties are specified as strings, this is vunerable to code changes
+            Assert.That(comparisons, Has.Exactly(1)
+                .Property("ProductName").EqualTo("a")
+                .And
+                .Property("InterestRate").EqualTo(1)
+                .And
+                .Property("MonthlyRepayment").GreaterThan(0));
+
+            // Same test but using type and lambda
+            Assert.That(comparisons, Has.Exactly(1)
+                .Matches<MonthlyRepaymentComparison>(
+                    item => item.ProductName == "a" &&
+                            item.InterestRate == 1 &&
+                            item.MonthlyRepayment > 0));
+        }
     }
 }
